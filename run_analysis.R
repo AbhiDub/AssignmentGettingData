8a67dae9908bf1da1947cec6e460c5302623d72e
AbhiDub <geodubey@gmail.com>
2017-01-31 15:56:15 +0530

    Updating script

--- run_analysis.R
+++ run_analysis.R
@@ -1,95 +1,79 @@
-## Downloaded the data into a test folder
-download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","test")
-temp <- tempfile()
-unzip(temp, list = TRUE)
-YTest <- read.table(unzip(temp, "UCI HAR Dataset/test/y_test.txt"))
-XTest <- read.table(unzip(temp, "UCI HAR Dataset/test/X_test.txt"))
-SubjectTest <- read.table(unzip(temp, "UCI HAR Dataset/test/subject_test.txt"))
-YTrain <- read.table(unzip(temp, "UCI HAR Dataset/train/y_train.txt"))
-XTrain <- read.table(unzip(temp, "UCI HAR Dataset/train/X_train.txt"))
-SubjectTrain <- read.table(unzip(temp, "UCI HAR Dataset/train/subject_train.txt"))
-Features <- read.table(unzip(temp, "UCI HAR Dataset/features.txt"))
 
-## Fixing column names
+## Downloading and unzipping 
 
-colnames(XTrain) <- t(Features[2])
-colnames(XTest) <- t(Features[2])
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
 
-##Merge should align
+# Unzip dataSet to /data directory
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
-XTrain$activities <- YTrain[, 1]
-XTrain$participants <- SubjectTrain[, 1]
-XTest$activities <- YTest[, 1]
-XTest$participants <- SubjectTest[, 1]
+## Merging the training and the test sets :
+## Reading files
 
-## Merging the dataset
+# Reading the train tables:
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 
-Master <- rbind(XTrain, XTest)
-duplicated(colnames(Master))
-Master <- Master[, !duplicated(colnames(Master))]
+# Reading test tables:
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
-## Assignment 2 - extract only measurements on mean and standard deviation
-Mean <- grep("mean()", names(Master), value = FALSE, fixed = TRUE)
-InstrumentMeanMatrix <- Master[Mean]
-# For STD
-STD <- grep("std()", names(Master), value = FALSE)
-InstrumentSTDMatrix <- Master[STD]
+# Reading feature vector:
+features <- read.table('./data/UCI HAR Dataset/features.txt')
 
-## Changing the class
+# Reading activity labels:
+activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
 
-Master$activities <- as.character(Master$activities)
-Master$activities[Master$activities == 1] <- "Walking"
-Master$activities[Master$activities == 2] <- "Walking Upstairs"
-Master$activities[Master$activities == 3] <- "Walking Downstairs"
-Master$activities[Master$activities == 4] <- "Sitting"
-Master$activities[Master$activities == 5] <- "Standing"
-Master$activities[Master$activities == 6] <- "Laying"
-Master$activities <- as.factor(Master$activities)
+## Assigning column names:
 
-##Using gsub to replace the list
+colnames(x_train) <- features[,2] 
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
+      
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+      
+colnames(activityLabels) <- c('activityId','activityType')
 
-names(Master) <- gsub("Acc", "Accelerator", names(Master))
-names(Master) <- gsub("Mag", "Magnitude", names(Master))
-names(Master) <- gsub("Gyro", "Gyroscope", names(Master))
-names(Master) <- gsub("^t", "time", names(Master))
-names(Master) <- gsub("^f", "frequency", names(Master))
-## Changing the participant names
+## Merging all data in one set:
 
-Master$participants <- as.character(Master$participants)
-Master$participants[Master$participants == 1] <- "Participant 1"
-Master$participants[Master$participants == 2] <- "Participant 2"
-Master$participants[Master$participants == 3] <- "Participant 3"
-Master$participants[Master$participants == 4] <- "Participant 4"
-Master$participants[Master$participants == 5] <- "Participant 5"
-Master$participants[Master$participants == 6] <- "Participant 6"
-Master$participants[Master$participants == 7] <- "Participant 7"
-Master$participants[Master$participants == 8] <- "Participant 8"
-Master$participants[Master$participants == 9] <- "Participant 9"
-Master$participants[Master$participants == 10] <- "Participant 10"
-Master$participants[Master$participants == 11] <- "Participant 11"
-Master$participants[Master$participants == 12] <- "Participant 12"
-Master$participants[Master$participants == 13] <- "Participant 13"
-Master$participants[Master$participants == 14] <- "Participant 14"
-Master$participants[Master$participants == 15] <- "Participant 15"
-Master$participants[Master$participants == 16] <- "Participant 16"
-Master$participants[Master$participants == 17] <- "Participant 17"
-Master$participants[Master$participants == 18] <- "Participant 18"
-Master$participants[Master$participants == 19] <- "Participant 19"
-Master$participants[Master$participants == 20] <- "Participant 20"
-Master$participants[Master$participants == 21] <- "Participant 21"
-Master$participants[Master$participants == 22] <- "Participant 22"
-Master$participants[Master$participants == 23] <- "Participant 23"
-Master$participants[Master$participants == 24] <- "Participant 24"
-Master$participants[Master$participants == 25] <- "Participant 25"
-Master$participants[Master$participants == 26] <- "Participant 26"
-Master$participants[Master$participants == 27] <- "Participant 27"
-Master$participants[Master$participants == 28] <- "Participant 28"
-Master$participants[Master$participants == 29] <- "Participant 29"
-Master$participants[Master$participants == 30] <- "Participant 30"
-Master$participants <- as.factor(Master$participants)
+mrg_train <- cbind(y_train, subject_train, x_train)
+mrg_test <- cbind(y_test, subject_test, x_test)
+setAllInOne <- rbind(mrg_train, mrg_test)
 
-## Creating final dataset
-Master.dt <- data.table(Master)
+##Extracting only the measurements on the mean and standard deviation 
+## Reading column names:
 
-TidyData <- Master.dt[, lapply(.SD, mean), by = 'participants,activities']
-write.table(TidyData, file = "Tidy.txt", row.names = FALSE)
+colNames <- colnames(setAllInOne)
+
+## Create vector for defining ID, mean and standard deviation:
+
+mean_and_std <- (grepl("activityId" , colNames) | 
+                 grepl("subjectId" , colNames) | 
+                 grepl("mean.." , colNames) | 
+                 grepl("std.." , colNames) 
+                 )
+
+## Making subset :
+
+setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+
+## Naming descriptive activity names :
+
+setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
+                              by='activityId',
+                              all.x=TRUE)
+
+## Creating a tidy data set with the average of each variable for each activity and each subject:
+
+secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
+
+## Writing tidy data set 
+
+write.table(secTidySet, "secTidySet.txt", row.name=FALSE)
+
